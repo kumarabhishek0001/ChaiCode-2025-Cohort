5e592b14a2974740f9ab72727b35bd4eee28cbc1
Abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2025-06-25 11:50:24 +0530

    # Using ESlint and Prettier - containes configuration of prettier and ESlint - Alternatively use Biom

---
+++ React/01-06-2025/Chapter-03/src/App.js
@@ -0,0 +1,23 @@
+import React from 'https://esm.sh/react@19.1.0';
+import ReactDOM from 'https://esm.sh/react-dom@19.1.0/client';
+
+const chai = (props) => {
+  console.log(props.name, props.cost);
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, props.name),
+    React.createElement('p', {}, props.cost),
+  ]);
+};
+
+const App = () => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Chai Variations'),
+    React.createElement(chai, { name: 'Ginger Tea', cost: 700 }),
+    React.createElement(chai, { name: 'Lemon Tea', cost: 850 }),
+    React.createElement(chai, { name: 'Masala Chai', cost: 1000 }),
+  ]);
+};
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
