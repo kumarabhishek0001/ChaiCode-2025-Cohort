ec0f5c0cc233b7db715f658bee7a68f34900fa15
Abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2025-06-25 13:20:32 +0530

    # [Chapter-5] Using vite from scratch

---
+++ React/01-06-2025/Chapter-05/src/App.jsx
@@ -0,0 +1,8 @@
+export function App(){
+    return(
+        <div>
+            <h1>Welcome to chai code</h1>
+            <p>Serving Hot chai with react</p>
+        </div>
+    )
+}
---
+++ React/01-06-2025/Chapter-05/vite.config.js
@@ -0,0 +1,7 @@
+//vite confing js
+import { defineConfig } from "vite";
+import React from "@vitejs/plugin-react";
+
+export default defineConfig({
+    plugins: [React()]
+})
