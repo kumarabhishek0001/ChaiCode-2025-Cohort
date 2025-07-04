11dd20cd6bd3e2bc3a99473991e31ea1b4ee3da6
abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2025-07-04 15:05:12 +0530

    [22-02-2025] Math & String

---
+++ JavaScript/Crash-Course/22-02-2025/math_&_strings.js
@@ -0,0 +1,29 @@
+// console.log(Math.max(1,30,65,11,54))
+// console.log(Math.min(1,30,65,11,54))
+
+// console.log(Math.random())
+
+// console.log(Math.random()*10)
+//create a funciton that reutrn number from 1 to 6
+
+
+//* string
+let firstName = 'abhishek';
+let lastName = 'kumar'
+
+// console.log(firstName + " " + lastName)
+
+//* string methods
+const message = 'Hello world!'
+console.log(message.length)
+
+console.log(message.toUpperCase())
+console.log(message.toLowerCase())
+
+console.log(message.indexOf('wo'))
+console.log(message.slice(0,5))
+
+
+//template litreals 
+let myName = "Abhishek"
+console.log(`my name is ${myName}`)
