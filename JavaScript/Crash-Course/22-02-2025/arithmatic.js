610962710ee7ce3b5e04f82496fb97cecbf6537e
abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2025-07-04 13:33:46 +0530

    [22-02-2025]  Arithmatic operations on Datatypes

---
+++ JavaScript/Crash-Course/22-02-2025/arithmatic.js
@@ -0,0 +1,12 @@
+//* use paranthesis in arithmatic operations
+
+let a = 1;
+let b = 24;
+let c = 6
+
+const result = (a*b)/c //don't do a*b/c
+
+//* quotient
+let quotient = b/c
+let remaider = b/5
+let power = 24**6
